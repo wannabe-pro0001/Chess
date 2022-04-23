323d0c52a18de82ee3b3ea5de80275004b036451
Wannabe-pro0001 <you@example.com>
2022-04-23 09:58:54 +0700

    yes

--- MoveFinder.py
+++ MoveFinder.py
@@ -9,3 +9,12 @@ def FindRandomMove(validMove):
 
 def FindBestMove():
     return 
+
+#evaluation 
+'''
+pieceVal = {'K' : 0, 'Q' : 9, 'R': 6, 'B' : 3, 'N': 3, 'p' : 1}
+score += pieceVal[piece] * ([piece - piece']) #piece' stand for black other is white
+        + 0.5(M - M')
+        +
+
+'''
